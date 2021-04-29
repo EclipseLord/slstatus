ef935130f1768308f35ea9ad86960c06bd0f18c3
EclipseLord <aidenmonkeybutt@live.com>
2021-04-29 11:06:01 -0700

    bruh

--- config.def.h
+++ config.def.h
@@ -65,5 +65,9 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ datetime, "%s",           "%F %T" },
+        {cpu_perc, "[CPU %s%]  ", NULL},
+        {swap_total, "[SWAP: %s]  ", NULL},
+        {ram_used, "[RAM: %s/", NULL},
+        {ram_total, "%s]  ", NULL},
+        {datetime,  "%s", "%F %T"},
 };
